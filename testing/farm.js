cdac5968e5ee1746821f2fb7f439dd424f8d0de6
Janelle Tavares <janelletavares@users.noreply.github.com>
2024-01-26 09:42:08 +0100

    Finish farm script and convert to headless (#9)

--- testing/farm.js
+++ testing/farm.js
@@ -7,82 +7,67 @@
 
 
 export DISPLAY_NAME=
-export ZOOM_USERNAME=
-export ZOOM_PASSWORD=
+export SESSION_LENGTH_SECONDS=
 export ZOOM_MEETING_ID=
 export ZOOM_MEETING_PASSCODE=
 
 node farm.js
  */
 
-
-// Require selenium webdriver
-let webdriver= require("selenium-webdriver");
-const {until, By} = require("selenium-webdriver");
-
-// Build new window of chrome
-let driver =  new webdriver.Builder()
-    .withCapabilities(webdriver.Capabilities.chrome())
-   .forBrowser("firefox").
-    build();
+const {until, By, Browser, Builder} = require("selenium-webdriver");
+const firefox = require('selenium-webdriver/firefox');
 
 const displayName = process.env.DISPLAY_NAME
-const username = process.env.ZOOM_USERNAME
-const password = process.env.ZOOM_PASSWORD
+const sessionSeconds = process.env.SESSION_LENGTH_SECONDS
 const meetingID = process.env.ZOOM_MEETING_ID
 const meetingPasscode = process.env.ZOOM_MEETING_PASSCODE
-const actions = driver.actions({bridge: true });
 
 // close cookies popup
-function closePopups() {
-    rejectCookiesButton = driver.wait(until.elementIsEnabled(driver.findElement(By.css('button#onetrust-reject-all-handler'), 10000)));
-    actions.click(rejectCookiesButton).perform();
-     console.log('Pop ups all gone');
+async function closeCookiesPopup(driver) {
+    await driver.wait(until.elementLocated(By.id("onetrust-reject-all-handler")), 2000)
+    await driver.findElement(By.id('onetrust-reject-all-handler')).click();
+    console.log('cookie pop up gone');
 }
 
-function login() {
-    closePopups()
-    emailField = driver.wait(until.elementLocated(By.id("email")), 10000);
-    emailField.sendKeys(username)
-
-    passwordField = driver.wait(until.elementLocated(By.id("password")), 10000);
-    passwordField.sendKeys(password)
-
-    signInButton = driver.wait(until.elementLocated(By.xpath('//*[@id="js_btn_login"]')), 10000);
-    actions.click(signInButton).perform();
+async function termsAndPolicies(driver) {
+    await driver.findElement(By.id('wc_agree1')).click();
+    console.log('terms pop up gone');
 }
 
-function termsAndPolicies() {
-    agreeButton = driver.wait(until.elementIsEnabled(driver.findElement(By.css('#wc_continue'), 10000)));
-    actions.click(agreeButton).perform();
-}
-function joinMeeting() {
-    inputname = driver.wait(until.elementLocated(By.id("input-for-name")), 1000)
-    inputname.clear()
+async function joinMeeting(driver) {
+    let inputname = await driver.wait(until.elementLocated(By.id("input-for-name")), 4000)
+    await inputname.clear()
     inputname.sendKeys(displayName)
 
-    inputpasscode = driver.wait(until.elementLocated(By.id("input-for-pwd")), 10000)
-    inputpasscode.sendKeys(meetingPasscode)
+    let inputpasscode = await driver.wait(until.elementLocated(By.id("input-for-pwd")), 1000)
+    await inputpasscode.sendKeys(meetingPasscode)
 
-    /*
-    doesn't work :-(
-    driver.sleep(5000).then(() => {
-        joinButton = driver.wait(until.elementIsEnabled(driver.findElement(By.css('html body div.main div#root div.preview-root div.preview-new-flow div.preview-new-flow-content div.preview-meeting-info button.zm-btn.preview-join-button.zm-btn--default.zm-btn__outline--blue'), 10000)));
-        actions.click(joinButton).perform();
-    })
-    */
+    let joinXPath = '/html/body/div[2]/div[2]/div/div[1]/div/div[2]/button'
+    await driver.wait(until.elementIsEnabled(driver.findElement(By.xpath(joinXPath), 1000))).click();
+
+    console.log("connected and waiting...")
+    await driver.sleep(sessionSeconds * 1000)
+    console.log("done waiting")
 }
 
-driver.get('https://zoom.us/signin#/login').then(() => {
-    driver.sleep(3000).then(() => {
-        login()
-        driver.sleep(10000).then(() => {
-            driver.get('https://zoom.us/wc/' + meetingID + '/join').then(() => {
-                driver.sleep(5000).then(() => {
-                    joinMeeting();
-                });
-            });
-        })
-    })
-})
+async function createParticipant(driver) {
+    await driver.get('https://zoom.us/wc/' + meetingID + '/join').then(async () => {
+        await closeCookiesPopup(driver);
+        await driver.sleep(1000).then(async () => {
+            await termsAndPolicies(driver);
+            await joinMeeting(driver);
+        });
+    });
+}
 
+(async function main() {
+    let fOps = new firefox.Options().headless() // deprecated, but it works
+    let driver = new Builder()
+        .setFirefoxOptions(fOps)
+        .forBrowser(Browser.FIREFOX).build();
+    try {
+        await createParticipant(driver);
+    } finally {
+        await driver.quit();
+    }
+})();
